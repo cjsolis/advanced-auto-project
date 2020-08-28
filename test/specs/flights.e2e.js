79b17bd886a6f4fe3ae57e3ec36834d78e875565
Christopher Solis <christopher.solis@edify.cr>
2020-08-28 13:28:41 -0600

    Added comments

--- test/specs/flights.e2e.js
+++ test/specs/flights.e2e.js
@@ -3,16 +3,17 @@ const FlightPage = require('../pageobjects/flight.page');
 describe('Flight page', () => {
     it('should verify that the given URL has unavailable results on Only Bus and available results on Bus and Train results', () => {
 
-        let loadingDiv = $('//div[contains(@class, "LoadingProvidersstyled__StyledLoadingProviders")]');
-
+        // Opens the page and verifies that there are results
         FlightPage.openFlightsWithPassengers();
         FlightPage.waitForResultsToLoad();
         expect(FlightPage.btnBook).toExist();
         
+        // Filters by Bus Only and verifies that there are no results
         FlightPage.selectTransportOnlyByName("Bus");
         FlightPage.waitForResultsToLoad();
         expect(FlightPage.btnBook).not.toExist();
         
+        // Adds Train to the filter and verifies that there are results
         FlightPage.selectTransportByName("Train");
         FlightPage.waitForResultsToLoad();
         expect(FlightPage.btnBook).toExist();        
